4fe086597f19da2f0ac39b68708d9857b62d33db
David Lynch <lynchda78@gmail.com>
2020-10-16 01:47:48 +0000

    Almost complete code

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 def removePun(word):
 	c = ",.?!;:"
 	new_list = list(word)
+
 	for count in range(0,len(word)):
 		for countt in range(0,len(c)):
 			if c[countt] in word[count]:
@@ -12,3 +13,42 @@ def removePun(word):
 		new_word += let
 
 	return new_word
+
+def decode(s):
+	if s == "":
+		return ""
+	else:
+		return chr(ord(s[0])-5) + decode(s[1:])
+
+def encode(s):
+	if s == "":
+		return ""
+	else:
+		return chr(ord(s[0])+5)+ encode(s[1:])
+
+def stringList(str1):
+	return str1.split()
+
+def encodeM(lst, cnt = 0):
+	last_elem = lst[-1]
+	if lst[cnt] == last_elem:
+		return encode(lst[cnt])
+	else:
+		return [encode(lst[cnt])] + [encodeM(lst,cnt+1)]
+
+def decodeM(lst, cnt = 0):
+	last_elem = lst[-1]
+	if lst[cnt] == last_elem:
+		return decode(lst[cnt])
+	else:
+		return [decode(lst[cnt])] + [decodeM(lst,cnt+1)]
+
+def main(s):
+	slst=removePun(s)
+	eList = encodeM(stringList(slst))
+	dList = decodeM(eList)
+	print("Given string =>", s)
+	print("Punctuation removed =>", slst)
+	print("List Encoded =>", eList)
+	print("List Decoded =>", dList)
+	print ("Encoded Message =>",' '.join(eList))
